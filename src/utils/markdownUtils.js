8371aedc4b52d3f0ddef2f94519c2a3b75fbc0e5
TheSpeedCubing <git@speedcubing.top>
2025-04-19 12:11:11 +0200

    let markdown target blank

--- src/utils/markdownUtils.js
+++ src/utils/markdownUtils.js
@@ -5,10 +5,18 @@ import rehypeSanitize from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
 import './markdown.css';
 
+function LinkRenderer(props) {
+  return (
+    <a href={props.href} target="_blank" rel="noreferrer">
+      {props.children}
+    </a>
+  );
+}
 
 export function renderMarkdown(markdown) {
     return (
         <Markdown 
+          components={{a: LinkRenderer}}
           remarkPlugins={[remarkGfm]}
           rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeHighlight]}
         >
