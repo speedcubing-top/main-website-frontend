0fd4625c12bacf6c4482568ce9ca859202c139c4
Windows On The World <01wtcplaza@gmail.com>
2025-05-01 01:41:10 +0800

    Add notes/* routes

---
+++ pages/notes/[[...slug]].js
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout';
+import Notes from '../../components/Notes';
+
+function Page() {
+  return (
+    <Layout>
+      <Notes />
+    </Layout>
+  );
+}
+
+export default Page;
+
